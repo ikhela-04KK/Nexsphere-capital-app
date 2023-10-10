8094c39e56449cc54aa1b544b4bb4cbf9a1b8964
lin0410 <142158509+lin0410@users.noreply.github.com>
2023-10-10 15:36:47 +0000

    design the navbar

---
+++ nexsphere-capital-app/app/font.ts
@@ -0,0 +1,9 @@
+// creation des fonts 
+import {Inter} from 'next/font/google'; 
+
+// process pour exporter un fonct depuis
+export const inter = Inter({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: '900'
+}); 
